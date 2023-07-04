e59af93f9b0c1a5d9c957ed90c1b0e89f609f0ed
andriana777 <andrianaprokopets@gmail.com>
2023-07-04 09:08:39 +0300

    Empty check

--- resources/views/pages/tasks/task-info.blade.php
+++ resources/views/pages/tasks/task-info.blade.php
@@ -5,26 +5,30 @@
 <div class="container">
 
 <div>
-<p>{{$task->title}}</p>
+<p>{{$task->title ?? ''}}</p>
 </div>
 <div>
-<p>{{$task->description}}</p>
+<p>{{$task->description ?? ''}}</p>
 </div>
+    @if(!empty($task->user->name))
 <div>
     <p>Username:</p>
 <p>{{$task->user->name}}</p>
 </div>
+    @endif
+    @if(!empty($task->status->name))
 <div>
     <p>Status:</p>
     <p>{{$task->status->name}}</p>
 </div>
+    @endif
 <div>
     <p>Created:</p>
-    <p><span>{{$task->created_at}}</p>
+    <p><span>{{$task->created_at ?? ''}}</p>
 </div>
 <div>
     <p>Updated:</p>
-    <p>{{$task->updated_at}}</p>
+    <p>{{$task->updated_at ?? ''}}</p>
 </div>
 
 
--- resources/views/pages/users/user-info.blade.php
+++ resources/views/pages/users/user-info.blade.php
@@ -7,18 +7,21 @@
 <div>
 <h3>{{$user->name}}</h3>
 </div>
-
+@if(!empty($user->role->name))
 <div>
     <h4>Role:</h4>
-<p>{{$user->role}}</p>
+<p>{{$user->role->name}}</p>
 </div>
+    @endif
+    @if(!empty($user->email))
 <div>
     <p>Email:</p>
     <p>{{$user->email}}</p>
 </div>
+    @endif
 <div>
     <p>Created:</p>
-    <p><span>{{$user->created_at}}</p>
+    <p><span>{{$user->created_at ?? ''}}</p>
 </div>
 
 
